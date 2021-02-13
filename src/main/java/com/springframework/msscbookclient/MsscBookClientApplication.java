69f12c5c759e2687152a67e89a64cb1fb4830e4e
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-13 17:18:10 -0500

    first commit

---
+++ src/main/java/com/springframework/msscbookclient/MsscBookClientApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.msscbookclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBookClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBookClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/msscbookclient/MsscBookClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.msscbookclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBookClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

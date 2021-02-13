0bc309ecdcd01f6ad951634d8021e2a6f5a8b43f
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-13 18:41:05 -0500

    RestTemplate usage

--- src/main/java/com/springframework/msscbookclient/web/client/BookClient.java
+++ src/main/java/com/springframework/msscbookclient/web/client/BookClient.java
@@ -1,7 +1,12 @@
 package com.springframework.msscbookclient.web.client;
 
+import com.springframework.msscbookclient.web.model.BookDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 /*
 @ConfigurationProperties
@@ -12,8 +17,20 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(value = "sfg.book", ignoreUnknownFields = false)
 public class BookClient {
-    private final String BOOK_PATH_V1 = "/api/v1/book";
+    private final String BOOK_PATH_V1 = "/api/v1/book/";
     private String apiHost;
+
+    private final RestTemplate restTemplate;
+
+    public BookClient(RestTemplateBuilder restTemplateBuilder) {
+        // uses an instance of RestTemplateBuilder to get the RestTemplate
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BookDto getBookById(UUID id) {
+        // 2nd argument is BookDto.class as we are expecting that
+        return restTemplate.getForObject(apiHost + BOOK_PATH_V1 + id.toString(), BookDto.class);
+    }
 
     public void setApiHost(String apiHost) {
         this.apiHost = apiHost;

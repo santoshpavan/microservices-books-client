1d538917c0906d06fb00b823362a122de536a7d0
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-13 21:08:07 -0500

    Integration Testing for POST

--- src/test/java/com/springframework/msscbookclient/web/client/BookClientTest.java
+++ src/test/java/com/springframework/msscbookclient/web/client/BookClientTest.java
@@ -4,6 +4,7 @@ import com.springframework.msscbookclient.web.model.BookDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,15 @@ class BookClientTest {
     void getBookById() {
         BookDto dto = client.getBookById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBook() {
+        // given book
+        BookDto bookDto = BookDto.builder().bookName("New Book").build();
+
+        URI uri = client.saveNewBook(bookDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
     }
 }
